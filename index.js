cf557e6075c0f0901d3c61907b71f794ea9b2e17
SerFio00 <fiorenzo.serpico@gmail.com>
2020-07-23 20:05:00 +0200

    aggiunta controllo lingue e lingua inglese

--- index.js
+++ index.js
@@ -4,6 +4,7 @@ const mysql = require("mysql")
 const key = require("./Resource/Api.json")
 
 const Credits = require("./Credits.json")
+const lingua = require("./lingua.json")
 var connection = mysql.createConnection({
     host: "localhost",
     user: "root",
@@ -16,48 +17,69 @@ connection.connect(err => {
     console.log("Connected to Database")
 })
 
-var Telegram = new Telegrambot(key.Apikey, { polling: true })
+var Telegram = new Telegrambot(key.Apikey, {
+    polling: true
+})
 
-function start(msg){
-    console.log("load")
-    connection.query(`SELECT * FROM Caduta WHERE ID = ${msg.chat.id}`, (err, res)=>{
-        if(err) throw err;
-        console.log(res.length)
-        if(res.length > 0){
-            Telegram.sendMessage(msg.chat.id, "AO, hai già mandato questo messaggio, basta!")
+function getLanguage(msg) {
+    return new Promise((resolve, reject) => {
+        let lanCode = msg.from.language_code
+        if (lanCode === 'en' || lanCode === 'it') {
+            resolve(lanCode)
             return
         }
-        connection.query(`INSERT INTO Caduta (ID) VALUES (?)`, [msg.from.id], err=>{
-            if(err) throw err;
-            Telegram.sendMessage(msg.chat.id, "Benvenuto " + msg.from.first_name + ", questo bot conta quante volte a @SoldatoRusso è caduta la penna")
+        resolve('en')
+    })
+}
+
+function start(msg) {
+    getLanguage(msg).then(code => {
+        let lang = lingua[code]
+        console.log("load")
+        connection.query(`SELECT * FROM Caduta WHERE ID = ${msg.chat.id}`, (err, res) => {
+            if (err) throw err;
+            console.log(res.length)
+            if (res.length > 0) {
+                Telegram.sendMessage(msg.chat.id, lang.registro)
+                return
+            }
+            connection.query(`INSERT INTO Caduta (ID) VALUES (?)`, [msg.from.id], err => {
+                if (err) throw err;
+                Telegram.sendMessage(msg.chat.id, lang.welcome.saluto + msg.from.first_name + lang.welcome.continuo)
+            })
         })
     })
 }
 
-Telegram.on("text", msg =>{
-    if(msg.text === "/start"){
-        start(msg)
-    }
-    if(msg.text === "/add" && msg.chat.id === 180923698){
-        connection.query(`UPDATE Caduta SET Contatore = Contatore+1 WHERE ID = ${msg.from.id}`, err =>  {
-            if (err) throw err
-            Telegram.sendMessage(msg.chat.id, "Aggiunto con successo, ora sei un po' più rincoglionito Fioré")
-        })
-    }
-    if(msg.text === "/reset" && msg.from.id === 180923698){
-        connection.query(`UPDATE Caduta SET Contatore = 0 WHERE ID = ${msg.from.id}`, err =>  {
-            if (err) throw err
-            Telegram.sendMessage(msg.chat.id, "Tutti i tuoi peccati sono stati assolti")
-        })
-    }
-    if(msg.text === "/show"){
-        connection.query(`SELECT * FROM Caduta WHERE ID = 180923698`, (err, src) =>  {
-            if (err) throw err
-            console.log(src)
-            Telegram.sendMessage(msg.chat.id, "Questo è il numero di volte nelle quali la penna è caduta: " + src[0].Contatore)
-        })
-    }
-    if(msg.text === "/credits"){
-        Telegram.sendMessage(msg.chat.id, `Questo bot è stato creato da: ${Credits.Fiorenzo} e basta :) \n \n \n OK OK, SCUSA NON MI PICCHIARE- \n \n \n Con l'aiuto di ${Credits.Stefano}`, {parse_mode:"HTML",})
-    }
+Telegram.on("text", msg => {
+    getLanguage(msg).then(code => {
+        let lang = lingua[code]
+        if (msg.text === "/start") {
+            start(msg)
+        }
+        if (msg.text === "/add" && msg.chat.id === 180923698) {
+            connection.query(`UPDATE Caduta SET Contatore = Contatore+1 WHERE ID = ${msg.from.id}`, err => {
+                if (err) throw err
+                Telegram.sendMessage(msg.chat.id, lang.aggiunto)
+            })
+        }
+        if (msg.text === "/reset" && msg.from.id === 180923698) {
+            connection.query(`UPDATE Caduta SET Contatore = 0 WHERE ID = ${msg.from.id}`, err => {
+                if (err) throw err
+                Telegram.sendMessage(msg.chat.id, lang.redenzione)
+            })
+        }
+        if (msg.text === "/show") {
+            connection.query(`SELECT * FROM caduta WHERE ID = 180923698`, (err, src) => {
+                if (err) throw err
+                console.log(src)
+                Telegram.sendMessage(msg.chat.id, lang.caduta + src[0].Contatore)
+            })
+        }
+        if (msg.text === "/credits") {
+            Telegram.sendMessage(msg.chat.id, lang.crediti.inizio + Credits.Fiorenzo + "\n \n \n" + lang.crediti.picchiare + "\n \n \n" + lang.crediti.scusa + Credits.Stefano, {
+                parse_mode: "HTML",
+            })
+        }
+    })
 })
